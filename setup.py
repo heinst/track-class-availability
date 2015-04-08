4011e237722b5d70dfe6e45c87661738d1c461aa
Trevor Heins <heinstrevorj@gmail.com>
2015-04-08 15:13:08 -0400

    Fixed BeautifulSoup4 Name

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='track-class-availability',
       version='1.0',
-      install_requires=['BeautifulSoup >= 4.3.0']
+      install_requires=['BeautifulSoup4 >= 4.3.0']
 )
